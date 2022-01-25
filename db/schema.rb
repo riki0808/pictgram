61d1a3816449fac956e6c1abaeaaf90d0eddd668
riki0808 <sr28329732@gmail.com>
2022-01-25 07:20:33 +0000

    add Rails04 task

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_063414) do
+ActiveRecord::Schema.define(version: 2022_01_24_162019) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
