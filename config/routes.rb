81f6ce73603ef697431914ed28427bdbed32c294
riki0808 <sr28329732@gmail.com>
2022-02-02 07:23:52 +0000

    add Rails07 task

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,11 @@ Rails.application.routes.draw do
   
   resources :users
   resources :topics
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  delete '/favorites', to: 'favorites#destroy'
+  
+  get '/comments/:topic_id/new', to: 'comments#new', as: 'comments'
+  post '/comments/:topic_id/create', to: 'comments#create', as: 'comments_create'
 end
